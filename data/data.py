5a7658942af74f490d83476e7731f8c77b8de6c9
esmaeil-mirvakili <smirvaki@ucsc.edu>
2025-06-20 13:43:53 -0700

    ddp training added

--- data/data.py
+++ data/data.py
@@ -3,9 +3,11 @@ import torch
 
 
 class DataLoaderLite:
-    def __init__(self, B, T):
+    def __init__(self, B, T, world_size=1, rank=0):
         self.B = B
         self.T = T
+        self.world_size = world_size
+        self.rank = rank
 
         with open('data/input.txt', 'r') as f:
             data = f.read()
@@ -13,7 +15,7 @@ class DataLoaderLite:
         enc = tiktoken.get_encoding('gpt2')
         tokens = enc.encode(data)
         self.tokens = torch.tensor(tokens)
-        self.current_pos = 0
+        self.current_pos = B * T * rank
 
     def __next__(self):
         buf = self.tokens[self.current_pos:self.current_pos + self.B * self.T + 1]
@@ -21,7 +23,7 @@ class DataLoaderLite:
         y = buf[1:]
         x = x.view(self.B, self.T)
         y = y.view(self.B, self.T)
-        self.current_pos += self.B * self.T
-        if self.current_pos + self.B * self.T + 1 > len(self.tokens):
-            self.current_pos = 0
+        self.current_pos += self.B * self.T * self.world_size
+        if self.current_pos + self.B * self.T * self.world_size + 1 > len(self.tokens):
+            self.current_pos = self.B * self.T * self.rank
         return x, y
